ea22b758d494f7ddc22820bb394232720be54c9f
SeokGeunCho <choseokgeun@gmail.com>
2025-04-07 21:42:33 +0900

    커밋

--- 19_CHOSEOKGEUN/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/B_while.java
+++ 19_CHOSEOKGEUN/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/B_while.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> cec8712 (왜 안돼~~)
 package com.ohgiraffers.section02.looping;
 
 import java.util.Scanner;
@@ -14,27 +10,10 @@ public class B_while {
         int i = 1;
 
         while(i <= 10) {
-<<<<<<< HEAD
-=======
-/* package com.ohgiraffers.section02.looping;
-
-public class B_while {
-    public void testSimpleWhile() {
-
-        // 초기식
-        int i = 1;
-        while (i <= 10) {
->>>>>>> 3cfb28b (수정2)
-=======
->>>>>>> cec8712 (왜 안돼~~)
             System.out.println(i);
             i++;
         }
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> cec8712 (왜 안돼~~)
 
     public void testWhileExample() {
 
@@ -49,18 +28,3 @@ public class B_while {
         System.out.println("시키는 데로 눌러줘서 감사합니다.");
     }
 }
-<<<<<<< HEAD
-=======
-    public void testSimpleWhile() {
-        char answer = '\0';
-        while (answer == 'Y' || asnwer == 'y') {
-            System.out.println("Y 또는 y 좀 눌러줄래?");
-            answer = sc.next().charAt();
-        }
-        System.out.println("시키는 데로 눌러줘서 감사합니다.");
-    }
-}
-*/
->>>>>>> 3cfb28b (수정2)
-=======
->>>>>>> cec8712 (왜 안돼~~)
